5f566a54b218d63a84b3963ff8b53086322b125a
amirejokar2 <jokar@hero.best>
2025-07-14 13:09:23 +0330

    feat : add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import BackToHomeCircle from "../components/ui/svg/BackToHomeCircle";
+export default function NotFound() {
+  return (
+    <div id="smooth-wrapper" className="mil-page-wrapper">
+      <div className="mil-transition-fade" id="swup">
+        <div className="mil-transition-frame">
+          <div id="smooth-content" className="mil-content">
+            <div className="mil-hero-4 mil-up" id="top">
+              <div id="scene" className="mil-hero-scene">
+                <div className="mil-anim-frame" data-depth="0.25">
+                  <img
+                    src="6.png"
+                    alt="shape image "
+                    className="mil-hero-animation"
+                    data-depth="0.25"
+                  />
+                </div>
+                <div className="mil-pad-frame" data-depth="0.35">
+                  <div className="mil-shape-pad"></div>
+                </div>
+              </div>
+              <div className="mil-overlay"></div>
+              <div className="container mil-hero-main mil-relative">
+                <div className="mil-hero-text">
+                  <i className="far fa-poop mil-mb15"></i>
+                  <p className="mil-stylized mil-m2 mil-mb30">oops!</p>
+                  <h1 className="mil-display1 mil-rubber mil-mb30">404</h1>
+                  <h1 className="mil-head2 mil-a1 mil-rubber">
+                    Looks like you got lost
+                  </h1>
+                  <div className="mil-circle-text-frame">
+                    <Link href="/" className="mil-circle-text mil-c-gone">
+                      <BackToHomeCircle />
+                      <span className="mil-arrow">
+                        <i
+                          className="fal fa-arrow-down"
+                          style={{ transform: "rotate(90deg)" }}
+                        ></i>
+                      </span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/ui/svg/BackToHomeCircle.tsx
@@ -0,0 +1,33 @@
+export default function BackToHomeCircle() {
+  return (
+    <>
+      <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 300 300"
+        xmlSpace="preserve"
+        data-value="360"
+        className="mil-rotate"
+      >
+        <defs>
+          <path
+            id="circlePath"
+            d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+          />
+        </defs>
+        <circle cx="150" cy="100" r="75" fill="none" />
+        <g>
+          <use xlinkHref="#circlePath" fill="none" />
+          <text style={{ letterSpacing: "1.7px" }}>
+            <textPath xlinkHref="#circlePath">
+              back to home - back to home - back to home -
+            </textPath>
+          </text>
+        </g>
+      </svg>
+    </>
+  );
+}
